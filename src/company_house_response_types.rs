1a082c4dd17c33d4b6dacba091ae58a0abdcd656
Ted Pilcher <tedspilcher@gmail.com>
2024-10-14 21:22:35 +0100

    Add OfficerListResponse struct

--- src/company_house_response_types.rs
+++ src/company_house_response_types.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 
@@ -79,6 +81,86 @@ pub struct OfficerItem {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct DateOfBirth {
+    pub day: Option<i32>,
     pub month: i32,
     pub year: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OfficerListResponse {
+    pub active_count: i32,
+    pub etag: String,
+    pub items: Option<Vec<OfficerListItem>>,
+    pub items_per_page: i32,
+    pub kind: Option<String>,
+    pub links: Links,
+    pub start_index: Option<i32>,
+    pub total_results: Option<i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OfficerListItem {
+    pub address: Address,
+    pub appointed_before: String,
+    pub appointed_on: NaiveDate,
+    pub contact_details: ContactDetails,
+    pub date_of_birth: DateOfBirth,
+    pub etag: String,
+    pub former_names: Vec<FormerNames>,
+    pub identification: Identification,
+    pub is_pre_1992_appointment: bool,
+    pub links: OfficerLinks,
+    pub name: String,
+    pub nationality: String,
+    pub occupation: String,
+    pub officer_role: String,
+    pub person_numer: String,
+    pub principal_office_address: PrincipalOfficerAddress,
+    pub resigned_on: NaiveDate,
+    pub responsibilities: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ContactDetails {
+    contact_name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FormerNames {
+    forenames: String,
+    surname: String,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Identification {
+    pub identification_type: String,
+    pub legal_authority: String,
+    pub legal_form: String,
+    pub place_registered: String,
+    pub registration_number: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OfficerLinks {
+    pub officer: Officer,
+    pub self_: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Officer {
+    pub appointments: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PrincipalOfficerAddress {
+    pub address_line_1: Option<String>,
+    pub address_line_2: Option<String>,
+    pub care_of: Option<String>,
+    pub country: Option<String>,
+    pub locality: Option<String>,
+    pub po_box: Option<String>,
+    pub postal_code: Option<String>,
+    pub premises: Option<String>,
+    pub region: Option<String>,
 }
