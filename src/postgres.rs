99cee5872f67c0afb2a05996e6de09db439a1499
Ted Pilcher <tedspilcher@gmail.com>
2024-11-02 16:07:16 +0000

    Add insert company fn

--- src/postgres.rs
+++ src/postgres.rs
@@ -1,8 +1,11 @@
 use sqlx::postgres::PgConnection;
 use uuid::Uuid;
+use sqlx::query;
+
 
 pub async fn init_db(conn: &mut PgConnection) -> Result<(), failure::Error> {
 
+    // TODO: refactor this into single transaction
     sqlx::query(
         r#"
         CREATE TABLE IF NOT EXISTS company (
@@ -30,11 +33,17 @@ pub async fn init_db(conn: &mut PgConnection) -> Result<(), failure::Error> {
 }
 
 
-pub fn store_company(company_house_number: &String) -> Uuid {
+pub async fn insert_company(conn: &mut PgConnection, company_house_id: &String) -> Result<Uuid, failure::Error> {
 
-    // generate UUID
+    let id: Uuid = Uuid::new_v4();
 
-    // store in db
-    
-    todo!()
+    query(
+        "INSERT INTO company (id, company_house_id) VALUES ($1, $2)"
+    )
+    .bind(id) // Bind the UUID to the first placeholder ($1)
+    .bind(company_house_id)    // Bind the name to the second placeholder ($2)
+    .execute(conn)
+    .await?;
+
+    Ok(id)
 }
