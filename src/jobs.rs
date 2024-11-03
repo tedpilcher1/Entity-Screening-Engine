da1a329aeca8ac10ed1849a908426179a37d9f26
Ted Pilcher <tedspilcher@gmail.com>
2024-11-03 14:52:02 +0000

    Start mapping out req jobs

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,14 +1,24 @@
-use crate::company_house_response_types::ShareholderList;
+use sqlx::PgConnection;
+use uuid::Uuid;
 
-pub fn get_sharholders(company_number: &String) -> ShareholderList {
-    todo!()
+use crate::postgres::insert_company;
+
+pub async fn process_search_request(conn: &mut PgConnection, company_house_id: &String){
+
+    // insert company into company table with uuid
+    let company_id = insert_company(conn, company_house_id).await.unwrap();
+
+    // produce message containing company_id and company_house_id
+    
 }
 
-pub fn get_shareholders_to_depth(company_number: &String, depth: i32) {
+pub fn get_shareholders(conn: &mut PgConnection, company_id: Uuid, company_house_id: &String) {
 
-    // get all shareholders of company_number
+    // get shareholers using company_house_id and company house api
 
-    // for each of those shareholders
+    // for each shareholder
 
-    // if company, get all shareholders to depth (depth - 1)
+        // if shareholder is company, store in company db
+
+        // if max depth not reached, produce message containing both company ids
 }
