029d6fce2914894d03f6b83024aae9e01ea5cfa3
Ted Pilcher <tedspilcher@gmail.com>
2024-11-03 17:27:29 +0000

    Update job.rs

--- src/jobs.rs
+++ src/jobs.rs
@@ -3,13 +3,11 @@ use uuid::Uuid;
 
 use crate::postgres::insert_company;
 
-pub async fn process_search_request(conn: &mut PgConnection, company_house_id: &String){
-
+pub async fn process_search_request(conn: &mut PgConnection, company_house_id: &String) {
     // insert company into company table with uuid
     let company_id = insert_company(conn, company_house_id).await.unwrap();
 
     // produce message containing company_id and company_house_id
-    
 }
 
 pub fn get_shareholders(conn: &mut PgConnection, company_id: Uuid, company_house_id: &String) {
@@ -18,7 +16,7 @@ pub fn get_shareholders(conn: &mut PgConnection, company_id: Uuid, company_house
 
     // for each shareholder
 
-        // if shareholder is company, store in company db
+    // if shareholder is company, store in company db
 
-        // if max depth not reached, produce message containing both company ids
+    // if max depth not reached, produce message containing both company ids
 }
