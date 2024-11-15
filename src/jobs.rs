0096f4ebf02a9baf453edad015c2c2b85bfd3365
Ted Pilcher <tedspilcher@gmail.com>
2024-11-15 20:44:19 +0000

    Refactor jobs and include example

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,22 +1,16 @@
-use sqlx::PgConnection;
+use failure::Fail;
 use uuid::Uuid;
 
-use crate::postgres::insert_company;
-
-pub async fn process_search_request(conn: &mut PgConnection, company_house_id: &String) {
-    // insert company into company table with uuid
-    let company_id = insert_company(conn, company_house_id).await.unwrap();
-
-    // produce message containing company_id and company_house_id
+pub enum Job {
+    TestJob(TestJob)
 }
 
-pub fn get_shareholders(conn: &mut PgConnection, company_id: Uuid, company_house_id: &String) {
+pub struct TestJob {
+    id: Uuid,
+}
 
-    // get shareholers using company_house_id and company house api
-
-    // for each shareholder
-
-    // if shareholder is company, store in company db
-
-    // if max depth not reached, produce message containing both company ids
+impl TestJob {
+    pub fn do_work() -> Result<(), failure::Error>{
+        todo!()
+    }
 }
