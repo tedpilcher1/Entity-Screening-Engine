10bfea606327fa388a3869e36a5c8efb46aef97d
Ted Pilcher <tedspilcher@gmail.com>
2024-11-15 20:28:39 +0000

    Refactor pulsar fn into structs

--- src/pulsar.rs
+++ src/pulsar.rs
@@ -10,6 +10,82 @@ pub struct CompanyId {
     company_house_id: String,
 }
 
+pub struct PulsarClient {
+    internal_client: Pulsar<TokioExecutor>,
+}
+
+impl PulsarClient {
+    pub async fn new() -> Self {
+        Self{
+            internal_client: Pulsar::builder(PULSAR_ADDR, TokioExecutor)
+            .build()
+            .await
+            .expect("Should be able to create new pulsar client builder"),
+        }   
+    }
+
+    pub async fn create_producer(&self) -> PulsarProducer {
+        PulsarProducer {
+            id: Uuid::new_v4(),
+            internal_producer: self.internal_client
+            .producer()
+            .with_topic("non-persistent://public/default/test")
+            .with_name("producer")
+            .with_options(producer::ProducerOptions {
+                schema: Some(proto::Schema {
+                    r#type: proto::schema::Type::String as i32,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+            .build()
+            .await
+            .expect("Should be able to create producer")
+        }
+    }
+
+    pub async fn create_consumer(&self) -> PulsarConsumer {
+        PulsarConsumer {
+            id: Uuid::new_v4(),
+            internal_consumer: self.internal_client.consumer()
+            .with_topic("non-persistent://public/default/test")
+            .with_consumer_name("consumer")
+            .with_subscription_type(SubType::Shared)
+            .with_subscription("test_subscription")
+            .build()
+            .await
+            .expect("Should be able to create consumer")
+        }
+    }
+}
+
+pub struct PulsarProducer {
+    id: Uuid,
+    internal_producer: Producer<TokioExecutor>,
+}
+
+impl PulsarProducer {
+    pub async fn produce_message(
+        &mut self,
+        company_id: Uuid,
+        company_house_id: String,
+    ) -> Result<(), failure::Error> {
+        self.internal_producer
+            .send(CompanyId {
+                company_id,
+                company_house_id,
+            })
+            .await?;
+    
+        Ok(())
+    }
+}
+
+pub struct PulsarConsumer {
+    id: Uuid,
+    internal_consumer: Consumer<CompanyId, TokioExecutor>
+}
+
 impl SerializeMessage for CompanyId {
     fn serialize_message(input: Self) -> Result<producer::Message, PulsarError> {
         let payload = serde_json::to_vec(&input).map_err(|e| PulsarError::Custom(e.to_string()))?;
@@ -26,55 +102,4 @@ impl DeserializeMessage for CompanyId {
     fn deserialize_message(payload: &Payload) -> Self::Output {
         serde_json::from_slice(&payload.data)
     }
-}
-
-pub async fn produce_message(
-    producer: &mut Producer<TokioExecutor>,
-    company_id: Uuid,
-    company_house_id: String,
-) -> Result<(), failure::Error> {
-    producer
-        .send(CompanyId {
-            company_id,
-            company_house_id,
-        })
-        .await?;
-
-    Ok(())
-}
-
-pub async fn get_pulsar_client() -> Pulsar<TokioExecutor> {
-    Pulsar::builder(PULSAR_ADDR, TokioExecutor)
-        .build()
-        .await
-        .expect("Should be able to create new pulsar client builder")
-}
-
-pub async fn get_producer(pulsar: &Pulsar<TokioExecutor>) -> Producer<TokioExecutor> {
-    pulsar
-        .producer()
-        .with_topic("non-persistent://public/default/test")
-        .with_name("producer")
-        .with_options(producer::ProducerOptions {
-            schema: Some(proto::Schema {
-                r#type: proto::schema::Type::String as i32,
-                ..Default::default()
-            }),
-            ..Default::default()
-        })
-        .build()
-        .await
-        .expect("Should be able to create producer")
-}
-
-pub async fn get_consumer(pulsar: &Pulsar<TokioExecutor>) -> Consumer<CompanyId, TokioExecutor> {
-    pulsar
-        .consumer()
-        .with_topic("non-persistent://public/default/test")
-        .with_consumer_name("consumer")
-        .with_subscription_type(SubType::Shared)
-        .with_subscription("test_subscription")
-        .build()
-        .await
-        .expect("Should be able to create consumer")
 }
