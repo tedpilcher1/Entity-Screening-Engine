a0d216d6e02d8b31acc214af1ed2d7d473ae8847
Ted Pilcher <tedspilcher@gmail.com>
2024-11-15 20:59:18 +0000

    Update Worker with new Database struct and refactor

--- src/worker.rs
+++ src/worker.rs
@@ -1,26 +1,25 @@
-use sqlx::PgConnection;
 use uuid::Uuid;
 
-use crate::{postgres::connect, pulsar::{PulsarClient, PulsarConsumer, PulsarProducer}};
+use crate::{postgres::Database, pulsar::{PulsarClient, PulsarConsumer, PulsarProducer}};
 
 pub struct Worker {
     id: Uuid,
-    conn: PgConnection,
+    database: Database,
     producer: PulsarProducer,
     consumer: PulsarConsumer,
 }
 
 impl Worker {
-    pub async fn new() -> Self {
+    pub async fn new() -> Result<Self, failure::Error> {
 
         let pulsar_client = PulsarClient::new().await;
 
-        Self {
+        Ok(Self {
             id: Uuid::new_v4(),
-            conn: connect().await,
+            database: Database::connect().await?,
             producer: pulsar_client.create_producer().await,
             consumer: pulsar_client.create_consumer().await,
-        }
+        })
     } 
 
     pub fn do_work() {
