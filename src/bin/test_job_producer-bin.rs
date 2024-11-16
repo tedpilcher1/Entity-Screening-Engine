16c4cac58e10a12dc220e8a666e30d696c4b63e2
Ted Pilcher <tedspilcher@gmail.com>
2024-11-16 17:24:29 +0000

    Add find_shareholders endpoint sim

--- src/bin/test_job_producer-bin.rs
+++ src/bin/test_job_producer-bin.rs
@@ -1,26 +1,30 @@
-use std::time::Duration;
-
-use tokio::time::sleep;
-use uuid::Uuid;
 use Company_Investigation::{
-    jobs::{Job, RecursiveShareholders},
-    pulsar::PulsarClient,
+    jobs::{Job, RecursiveShareholders}, postgres::Database, pulsar::PulsarClient
 };
 
-#[tokio::main]
-async fn main() {
+
+async fn simulate_find_shareholders_endpoint() {
+
+    let company_id = "02627406".to_string();
+    let depth = 3;
+
     let pulsar_client = PulsarClient::new().await;
     let mut producer = pulsar_client.create_producer().await;
 
-    loop {
-        let job = Job::RecursiveShareholders(RecursiveShareholders {
-            parent_id: Uuid::new_v4(),
-            parent_company_id: "02627406".to_string(),
-            remaining_depth: 3,
-        });
+    let mut conn = Database::connect().await.unwrap();
 
-        producer.produce_message(job).await.unwrap();
-        sleep(Duration::from_secs(20)).await;
-        break;
-    }
+    let parent_id = conn.insert_company(&company_id).await.unwrap();
+
+    let job = Job::RecursiveShareholders(RecursiveShareholders {
+        parent_id,
+        parent_company_id: company_id,
+        remaining_depth: depth,
+    });
+
+    producer.produce_message(job).await.unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    simulate_find_shareholders_endpoint().await
 }
