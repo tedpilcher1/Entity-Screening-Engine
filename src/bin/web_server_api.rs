251c79c430633256af82e237c85d8e2e9534bf1f
Ted Pilcher <tedspilcher@gmail.com>
2024-12-08 01:03:19 +0000

    Update cors to accept from anywhere

--- src/bin/web_server_api.rs
+++ src/bin/web_server_api.rs
@@ -225,15 +225,7 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     HttpServer::new(|| {
-        let cors = Cors::default()
-            .allowed_origin("https://www.rust-lang.org")
-            .allowed_origin_fn(|origin, _req_head| {
-                origin.as_bytes().ends_with(b".rust-lang.org")
-            })
-            .allowed_methods(vec!["GET", "POST"])
-            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-            .allowed_header(http::header::CONTENT_TYPE)
-            .max_age(3600);
+        let cors = Cors::default().allow_any_origin().send_wildcard();
         
         App::new()
             .wrap(cors)
