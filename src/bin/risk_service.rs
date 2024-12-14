6e612cbc9ccd8823117902bcec068b886536fb99
Ted Pilcher <tedspilcher@gmail.com>
2024-12-14 11:08:15 +0000

    Finish risk_service file to create worker and do work

--- src/bin/risk_service.rs
+++ src/bin/risk_service.rs
@@ -1,7 +1,12 @@
 use dotenv::dotenv;
+use Company_Investigation::workers::risk_worker::RiskWorker;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
     env_logger::init();
+    let mut worker = RiskWorker::new_worker()
+        .await
+        .expect("Should be able to create worker");
+    worker.do_work().await;
 }
