39b40c39c609049605d1b2b3e50b0f61ac0b8e35
Ted Pilcher <tedspilcher@gmail.com>
2024-12-14 11:07:28 +0000

    Add initial risk worker

--- src/workers/risk_worker.rs
+++ src/workers/risk_worker.rs
@@ -1,8 +1,47 @@
-use crate::postgres::Database;
+use pulsar::SubType;
 
+use crate::{jobs::jobs::Job, postgres::Database, pulsar::PulsarProducer};
 
+use super::worker::{Work, Worker};
 
+pub const RISK_TOPIC: &str = "non-persistent://public/default/risk";
+const SUBSCRIPTION: &str = "Risk-Sub";
+const SUB_TYPE: SubType = SubType::Shared;
 
-pub struct RiskWorker {
-    database: Database,
+pub struct RiskWorker {}
+
+impl RiskWorker {
+    pub async fn new_worker() -> Result<Worker<RiskWorker>, failure::Error> {
+
+        let risk_worker = RiskWorker{};
+        Ok(Worker::new(
+            RISK_TOPIC,
+            SUBSCRIPTION,
+            None,
+            None,
+            SUB_TYPE,
+            risk_worker,
+        )
+        .await?)
+    }
+}
+
+impl Work for RiskWorker {
+    async fn run_job(
+        &mut self,
+        job: Job,
+        database: &mut Database,
+        producer: &mut PulsarProducer,
+    ) -> Result<(), failure::Error> {
+        // let job_result = match job.job_kind {
+        //     _ => unimplemented!(),
+        // };
+
+        // database.complete_job(job.id)?;
+        // if let Err(_) = job_result {
+        //     database.update_job_with_error(&job.id)?
+        // }
+        // job_result?;
+        Ok(())
+    }
 }
