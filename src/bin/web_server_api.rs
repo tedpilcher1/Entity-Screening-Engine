0cd68d18cde5391bafba2b9ea9e56434b4d7f804
Ted Pilcher <tedspilcher@gmail.com>
2024-12-15 16:49:50 +0000

    Rename endpoints and expose flags, datasets and positions to /get_check

--- src/bin/web_server_api.rs
+++ src/bin/web_server_api.rs
@@ -13,7 +13,7 @@ use Company_Investigation::{
         jobs::JobKind,
         relation_jobs::{RelationJob, RelationJobKind},
     },
-    models::{Entity, Relationshipkind},
+    models::{Entity, Flagkind, Relationshipkind},
     postgres::Database,
     pulsar::PulsarClient,
     workers::entity_relation_worker::ENTITY_RELATION_TOPIC,
@@ -21,7 +21,53 @@ use Company_Investigation::{
 
 const MAX_DEPTH: usize = 3;
 
-fn get_entity_response(check_id: Uuid) -> Result<EntityResponse, failure::Error> {
+#[derive(Serialize, Deserialize)]
+struct CheckInfo {
+    check_id: Uuid,
+    entity_number: String,
+    name: Option<String>,
+    instructed_on: NaiveDateTime,
+    completed_on: Option<NaiveDateTime>,
+    risk_level: String, // TODO: change when implemented
+    flags: Vec<String>, // TODO: change when implemented
+}
+
+#[derive(Serialize, Deserialize)]
+struct ChecksResponse {
+    checks: Vec<CheckInfo>,
+    has_error: bool,
+}
+
+#[derive(Deserialize)]
+struct StartRelationsCheckParams {
+    relations_depth: Option<usize>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Relation {
+    entity_id: Uuid,
+    started_on: Option<NaiveDate>,
+    ended_on: Option<NaiveDate>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct EntityWithRelations {
+    entity: Entity,
+    officers: Vec<Relation>,
+    shareholders: Vec<Relation>,
+    flags: Vec<Flagkind>,
+    positions: Vec<String>,
+    datasets: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct EntityCheckResponse {
+    entities: Vec<EntityWithRelations>,
+    started_at: NaiveDateTime,
+    completed_at: Option<NaiveDateTime>,
+}
+
+fn get_entity_response(check_id: Uuid) -> Result<EntityCheckResponse, failure::Error> {
     let mut database = Database::connect()?;
     let mut entities: Vec<EntityWithRelations> = vec![];
     let check = database.get_check(check_id)?;
@@ -30,6 +76,11 @@ fn get_entity_response(check_id: Uuid) -> Result<EntityResponse, failure::Error>
     for entity in check_entities {
         let officers = database.get_relations(entity.id, Relationshipkind::Officer)?;
         let shareholders = database.get_relations(entity.id, Relationshipkind::Shareholder)?;
+
+        let flags = database.get_flag_kinds(&entity.id)?;
+        let positions = database.get_positions(&entity.id)?;
+        let datasets = database.get_positions(&entity.id)?;
+
         entities.push(EntityWithRelations {
             entity,
             officers: officers
@@ -48,54 +99,20 @@ fn get_entity_response(check_id: Uuid) -> Result<EntityResponse, failure::Error>
                     ended_on: shareholder.2,
                 })
                 .collect(),
+            flags,
+            positions,
+            datasets,
         })
     }
 
-    Ok(EntityResponse {
+    Ok(EntityCheckResponse {
         entities,
         started_at: check.started_at,
         completed_at: database.check_completed_at(check_id)?,
     })
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Relation {
-    entity_id: Uuid,
-    started_on: Option<NaiveDate>,
-    ended_on: Option<NaiveDate>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct EntityWithRelations {
-    entity: Entity,
-    officers: Vec<Relation>,
-    shareholders: Vec<Relation>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct EntityResponse {
-    entities: Vec<EntityWithRelations>,
-    started_at: NaiveDateTime,
-    completed_at: Option<NaiveDateTime>,
-}
-
-#[get("/get_relations/{check_id}")]
-async fn get_relations_endpoint(params: web::Path<Uuid>) -> impl Responder {
-    let check_id = params.into_inner();
-    match get_entity_response(check_id) {
-        Ok(entity_response) => HttpResponse::Ok().json(entity_response),
-        Err(e) => {
-            warn!("Failed to get relations: {}", e);
-            HttpResponse::InternalServerError()
-                .json(format!("Failed to get relations for check {}", check_id))
-        }
-    }
-}
-
-async fn start_relations_check(
-    company_house_number: String,
-    depth: usize,
-) -> Result<Uuid, failure::Error> {
+async fn start_check(company_house_number: String, depth: usize) -> Result<Uuid, failure::Error> {
     let mut database = Database::connect()?;
     let pulsar_client = PulsarClient::new().await;
     let mut producer = pulsar_client
@@ -144,44 +161,15 @@ async fn start_relations_check(
     Ok(check_id)
 }
 
-#[derive(Deserialize)]
-struct StartRelationsCheckParams {
-    relations_depth: Option<usize>,
-}
-
-#[post("/start_relations_check/{company_house_number}")]
-async fn start_relations_check_endpoint(
-    path: web::Path<String>,
-    info: Option<web::Query<StartRelationsCheckParams>>,
-) -> impl Responder {
-    let company_house_number = path.into_inner();
-
-    let depth = match info {
-        Some(info) => (info.relations_depth.unwrap_or_else(|| 0),),
-        None => (0,),
-    };
-
-    match start_relations_check(company_house_number.clone(), depth.0).await {
-        Ok(check_id) => HttpResponse::Ok().json(check_id),
-        Err(e) => {
-            warn!("Failed to get relations: {}", e);
-            HttpResponse::InternalServerError().json(format!(
-                "Failed to start relation check for entity with number {}",
-                company_house_number
-            ))
-        }
-    }
-}
-
 fn get_checks() -> Result<ChecksResponse, failure::Error> {
     let mut database = Database::connect().expect("Should be able to connect to db");
     let checks = database.get_checks()?;
-    let mut check_response_vec: Vec<CheckResponse> = Vec::new();
+    let mut check_response_vec: Vec<CheckInfo> = Vec::new();
     let mut has_error = false;
 
     for check in checks {
         let root_entity = database.get_root_entity(&check.id)?;
-        let check_response = CheckResponse {
+        let check_response = CheckInfo {
             check_id: check.id,
             entity_number: root_entity.company_house_number,
             name: root_entity.name,
@@ -203,32 +191,52 @@ fn get_checks() -> Result<ChecksResponse, failure::Error> {
     })
 }
 
+#[post("/start_check/{company_house_number}")]
+async fn start_check_endpoint(
+    path: web::Path<String>,
+    info: Option<web::Query<StartRelationsCheckParams>>,
+) -> impl Responder {
+    let company_house_number = path.into_inner();
+
+    let depth = match info {
+        Some(info) => (info.relations_depth.unwrap_or_else(|| 0),),
+        None => (0,),
+    };
+
+    match start_check(company_house_number.clone(), depth.0).await {
+        Ok(check_id) => HttpResponse::Ok().json(check_id),
+        Err(e) => {
+            warn!("Failed to get relations: {}", e);
+            HttpResponse::InternalServerError().json(format!(
+                "Failed to start relation check for entity with number {}",
+                company_house_number
+            ))
+        }
+    }
+}
+
+#[get("/get_check/{check_id}")]
+async fn get_check_endpoint(params: web::Path<Uuid>) -> impl Responder {
+    let check_id = params.into_inner();
+    match get_entity_response(check_id) {
+        Ok(entity_response) => HttpResponse::Ok().json(entity_response),
+        Err(e) => {
+            warn!("Failed to get relations: {}", e);
+            HttpResponse::InternalServerError()
+                .json(format!("Failed to get relations for check {}", check_id))
+        }
+    }
+}
+
 #[get("/get_checks")]
-async fn get_check_endpoint() -> impl Responder {
+async fn get_checks_endpoint() -> impl Responder {
     match get_checks() {
-        Ok(check_response) => HttpResponse::Ok().json(check_response),
+        Ok(checks_response) => HttpResponse::Ok().json(checks_response),
         Err(e) => {
             warn!("Failed to get checks: {}", e);
             HttpResponse::InternalServerError().json(format!("Failed to get checks"))
         }
     }
-}
-
-#[derive(Serialize, Deserialize)]
-struct CheckResponse {
-    check_id: Uuid,
-    entity_number: String,
-    name: Option<String>,
-    instructed_on: NaiveDateTime,
-    completed_on: Option<NaiveDateTime>,
-    risk_level: String, // TODO: change when implemented
-    flags: Vec<String>, // TODO: change when implemented
-}
-
-#[derive(Serialize, Deserialize)]
-struct ChecksResponse {
-    checks: Vec<CheckResponse>,
-    has_error: bool,
 }
 
 #[actix_web::main]
@@ -243,9 +251,9 @@ async fn main() -> std::io::Result<()> {
             .wrap(cors)
             // .app_data(web::Data::new(Database::connect()))
             // .app_data(web::Data::new(PulsarClient::new()))
-            .service(start_relations_check_endpoint)
-            .service(get_relations_endpoint)
+            .service(start_check_endpoint)
             .service(get_check_endpoint)
+            .service(get_checks_endpoint)
     })
     .bind("127.0.0.1:8080")?
     .run()
