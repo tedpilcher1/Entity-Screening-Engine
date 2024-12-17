2e91005fb0d7c8f8c3114fdb81ba608f7164b970
Ted Pilcher <tedspilcher@gmail.com>
2024-12-17 14:27:38 +0000

    Update streaming client to return stream of companies

--- src/company_house/company_house_streaming_client.rs
+++ src/company_house/company_house_streaming_client.rs
@@ -1,14 +1,15 @@
 use std::env;
 
-use futures::StreamExt;
-use reqwest::{header, Client};
+use bytes::Bytes;
+use futures::Stream;
 use lazy_static::lazy_static;
-
+use reqwest::{header, Client};
 
 const COMPANY_STREAMING_URL: &str = "https://stream.companieshouse.gov.uk/companies";
 
 lazy_static! {
-    static ref API_KEY: String = env::var("COMPANY_HOUSE_STREAMING_API_KEY").expect("Streaming API KEY should be set");
+    static ref API_KEY: String =
+        env::var("COMPANY_HOUSE_STREAMING_API_KEY").expect("Streaming API KEY should be set");
 }
 
 pub struct CompanyHouseStreamingClient {
@@ -22,25 +23,23 @@ impl CompanyHouseStreamingClient {
         }
     }
 
-    pub async fn connect_to_company_stream(&self) -> Result<(), failure::Error> {
-        
+    pub async fn connect_to_company_stream(
+        &self,
+    ) -> Result<impl Stream<Item = Result<Bytes, reqwest::Error>>, failure::Error> {
         let mut headers = header::HeaderMap::new();
         headers.insert(
             "Authorization",
             header::HeaderValue::from_str(&format!("{}", API_KEY.as_str()))?,
         );
-        
-        let mut stream = self.client
+
+        let stream = self
+            .client
             .get(COMPANY_STREAMING_URL)
             .headers(headers)
             .send()
             .await?
             .bytes_stream();
 
-        while let Some(item) = stream.next().await {
-            println!("Chunk: {:?}", item?);
-        }
-
-        Ok(())
+        Ok(stream)
     }
 }
