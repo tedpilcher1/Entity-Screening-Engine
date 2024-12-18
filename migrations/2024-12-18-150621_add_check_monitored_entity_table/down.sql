a1e0b8af5230d0b5e7e73ec1d3c5c099e77df876
Ted Pilcher <tedspilcher@gmail.com>
2024-12-18 15:49:51 +0000

    Fix add check monitored entity table migration

--- migrations/2024-12-18-150621_add_check_monitored_entity_table/down.sql
+++ migrations/2024-12-18-150621_add_check_monitored_entity_table/down.sql
@@ -1,17 +1,2 @@
 -- This file should undo anything in `up.sql`
-ALTER TABLE "check" DROP COLUMN "kind";
-
-
-
-
-
-
-
-
-
-
-
-
-
 DROP TABLE IF EXISTS "check_monitored_entity";
-DROP TABLE IF EXISTS "dormant_company";
